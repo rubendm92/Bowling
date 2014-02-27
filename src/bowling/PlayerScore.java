2cf8a8698be8b59e62bcca6ae61e2c7ce331a6c9
Rubén Díaz Martínez <rubendm92@gmail.com>
2014-02-27 11:32:23 +0000

    Remove FrameList class

--- src/bowling/PlayerScore.java
+++ src/bowling/PlayerScore.java
@@ -1,6 +1,8 @@
 package bowling;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class PlayerScore {
 
@@ -13,7 +15,7 @@ public class PlayerScore {
         this.scores = new int[21];
         this.size = 0;
     }
-    
+
     public String getPlayerName() {
         return playerName;
     }
@@ -23,34 +25,41 @@ public class PlayerScore {
     }
 
     public int getScore() {
-        return new FrameList().stream().reduce(0, (sum, frame) -> sum + frame.getPoints(), Integer::sum);
+        return getStream().reduce(0, (sum, frame) -> sum + frame.getPoints(), Integer::sum);
     }
-    
-    private class FrameList extends ArrayList<Frame> {
 
-        public FrameList() {
-            int index = 0;
-            while (new Frame(index).isCalculable()) {
-                add(new Frame(index));
-                index += (new Frame(index).isStrike() ? 1 : 2);
-            }
-        }
+    private Stream<Frame> getStream() {
+        return getFrameList().stream();
     }
-    
+
+    private ArrayList<Frame> getFrameList() {
+        ArrayList<Frame> result = new ArrayList<>();
+        int index = 0;
+        while (new Frame(index).isCalculable()) {
+            result.add(new Frame(index));
+            index += (new Frame(index).isStrike() ? 1 : 2);
+        }
+        return result;
+    }
+
     private class Frame {
 
         private final int index;
-        
+
         public Frame(int index) {
             this.index = index;
         }
-        
+
         public int getPoints() {
-            if (!isCalculable()) return 0;
-            if (isFull()) return scores[index] + scores[index + 1] + scores[index + 2];
+            if (!isCalculable()) {
+                return 0;
+            }
+            if (isFull()) {
+                return scores[index] + scores[index + 1] + scores[index + 2];
+            }
             return scores[index] + scores[index + 1];
         }
-        
+
         private boolean isCalculable() {
             return index + (isFull() ? 2 : 1) < size;
         }
